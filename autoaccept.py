865429094ffa16caf07f0f582dc0c0be0bf7f4d9
Simon <127450422+Smomsi@users.noreply.github.com>
2023-03-13 13:56:13 +0100

    updated the code (performance)
    Updated the code:
     updated the code to better performance hopefully :P
     noticed that this version already been updated so the image was no longer needed

--- autoaccept.py
+++ autoaccept.py
@@ -1,4 +1,3 @@
-#PROGRAMMED BY SIMON | 23.01.2022
 import pyautogui
 import time
 from threading import Thread
@@ -8,7 +7,12 @@ import requests
 from io import BytesIO
 
 response = requests.get("https://i.ibb.co/VxPb70F/acceptbuttonlol.png")
-img = Image.open(BytesIO(response.content))
+try:
+    img = Image.open(BytesIO(response.content))
+except:
+    print("Failed to load image")
+    exit()
+
 pyautogui.FAILSAFE=False
 
 def autoaccept():
@@ -18,7 +22,7 @@ def autoaccept():
     while "LeagueClientUxRender.exe" in (i.name() for i in psutil.process_iter()):
         time.sleep(1)
         r=None 
-        r=pyautogui.locateOnScreen(img, grayscale=False, confidence=0.8)
+        r=pyautogui.locateOnScreen(img, grayscale=False)
         if(r!=None):
             pyautogui.moveTo(r)
             time.sleep(0.1)
@@ -26,5 +30,9 @@ def autoaccept():
             time.sleep(0.1)
             continue
 
-t1 = Thread(target = autoaccept())
-t1.start
+def main():
+    t1 = Thread(target=autoaccept)
+    t1.start()
+
+if __name__ == '__main__':
+    main()
